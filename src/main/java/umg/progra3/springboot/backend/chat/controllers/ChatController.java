ef948536703a0f51323ae48dab25a861bee5a2cc
daranagt <daniel.aranagt@gmail.com>
2022-05-11 01:17:03 -0600

    Primer commit de la parte de Backend para la aplicación de chat del proyecto final de Programación 3

---
+++ src/main/java/umg/progra3/springboot/backend/chat/controllers/ChatController.java
@@ -0,0 +1,37 @@
+package umg.progra3.springboot.backend.chat.controllers;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import umg.progra3.springboot.backend.chat.models.documents.Mensaje;
+
+@Controller
+public class ChatController {
+	
+	private String[] colores = {"red","green","blue","magenta","purple","orange"};
+	
+	@MessageMapping("/mensaje")
+	@SendTo("/chat/mensaje")
+	public Mensaje recibeMensaje(Mensaje mensaje) {
+		mensaje.setFecha(new Date().getTime());
+		
+		if(mensaje.getTipo().equals("NUEVO_USUARIO")) {
+			mensaje.setColor(colores[new Random().nextInt(colores.length)]);
+			mensaje.setTexto("nuevo usuario");
+		}
+		
+		
+		return mensaje;
+	}
+	
+	@MessageMapping("/escribiendo")
+	@SendTo("/chat/escribiendo")
+	public String estaEscribiendo(String username) {
+		return username.concat(" está escribiendo ...");
+	}
+
+}
